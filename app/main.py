adbaaeeb9cd5eec326574aae7b45d6bd2bb4858f
wonjun0120 <wonjundero@gmail.com>
2025-01-08 20:57:26 +0900

    feat(SCRUM-114): cors 및 swagger 업데이트

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
-from app.routers import keyword_extractor
+from app.routers import extract_data
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app.include_router(keyword_extractor.router, prefix="/api", tags=["keyword extractor"])
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 도메인 허용 (개발 환경에서만 사용)
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
+
+app.include_router(extract_data.router, prefix="/api", tags=["extract data"])
 
 @app.get("/")
 async def root():
