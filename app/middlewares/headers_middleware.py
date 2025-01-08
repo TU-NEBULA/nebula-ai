f332455b05a046eb1ac1037108bdecca8269cb25
wonjun0120 <wonjundero@gmail.com>
2025-01-09 01:10:53 +0900

    feat(SCRUM-114): cors 헤더 추가 및 함수명 변경 통일

---
+++ app/middlewares/headers_middleware.py
@@ -0,0 +1,7 @@
+from starlette.middleware.base import BaseHTTPMiddleware
+
+class HeadersMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        response = await call_next(request)
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        return response
