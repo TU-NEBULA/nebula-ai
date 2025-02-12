e053f041447a31504564c7078ea605e4e54e414b
wonjun0120 <wonjundero@gmail.com>
2025-02-12 22:10:30 +0900

    feat(SCRUM-142): .env 세팅

--- app/core/database.py
+++ app/core/database.py
@@ -1,18 +1,15 @@
 from neo4j import GraphDatabase
-
-NEO4J_URI = 
-NEO4J_USER =
-NEO4J_PASSWORD = 
+from config import settings 
 
 driver = GraphDatabase.driver(
-    NEO4J_URI,
-    auth=(NEO4J_USER, NEO4J_PASSWORD)
+    settings.NEO4J_URI,
+    auth=(settings.NEO4J_USER, settings.NEO4J_PASSWORD)
 )
 
 def get_db_session():
     """
-    FastAPI 요청 처리 시마다 세션을 yield하는 함수.
-    with driver.session()을 통해 세션을 열고, 사용 완료 후 종료.
+    FastAPI 요청 처리 시마다 Neo4j 세션을 yield하는 함수.
+    with driver.session()을 통해 세션을 열고, 사용 완료 후 자동 종료.
     """
     with driver.session() as session:
         yield session
