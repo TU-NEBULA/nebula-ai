cec1d7667338e7a1e7a2da79b16a525885e8d28c
wonjun0120 <wonjundero@gmail.com>
2025-02-18 18:34:32 +0900

    feat(SCRUM-142): S3 키를 사용하여 HTML 문자열 임베딩 변환 로직 수정 및 todo 추가

--- app/main.py
+++ app/main.py
@@ -25,3 +25,8 @@ app.include_router(embedding.router, prefix="/api", tags=["embedding"])
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+# todo
+# todo 1: 키워드 추출 다시 확인
+# todo 2: 임베딩 시간 줄이기 -> 키워드, 사진 추출 이후 트리거, 임베딩 진행
--- app/services/embedding.py
+++ app/services/embedding.py
@@ -6,8 +6,7 @@ from app.utils.text_processing import extract_main_text, split_sentences
 def generate_embedding_from_s3(id: str, s3_key: str):
     """s3 키를 입력받아 HTML 문자열 임베딩 변환"""
 
-    # html_content = download_html_from_s3(s3_key)
-    html_content = s3_key
+    html_content = download_html_from_s3(s3_key)
         
     main_text = extract_main_text(html_content)
     main_text_embs = get_embedding(main_text)
