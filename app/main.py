c337226542c99f8076b028ccf79386ba7dea522b
wonjun0120 <wonjundero@gmail.com>
2025-03-05 20:59:42 +0900

    feat(SCRUM-159): 임베딩 체크 및 모든 임베딩 조회 기능 추가, Docker Compose에 볼륨 설정 추가

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routers import embedding
 from app.routers import extract_data
 from app.routers import embedding_status
+from app.routers import embed_check
 
 from app.middlewares.headers_middleware import HeadersMiddleware
 
@@ -34,6 +35,8 @@ app.add_middleware(HeadersMiddleware)
 app.include_router(embedding.router, prefix="/api", tags=["embedding"])
 app.include_router(extract_data.router, prefix="/api", tags=["extract_data"])
 app.include_router(embedding_status.router)
+app.include_router(embed_check.router, prefix="/api")
+
 
 @app.get("/")
 async def root():
